c02daf0ef5f3330c024c74c2332574835900cb39
mcintyre321 <mcintyre321@gmail.com>
2012-06-26 13:12:11 +0200

    Update master

--- SimpleScriptRunner/Properties/AssemblyInfo.cs
+++ SimpleScriptRunner/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleScriptRunner")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Command line tool for automatically diffing two SQL databases and producing numbered change scripts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("SimpleScriptRunner")]
